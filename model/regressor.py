0ce87afe6dd8ce545c290b4dbd540903be704b21
Adam J. Stewart <ajstewart426@gmail.com>
2018-06-07 12:06:44 -0500

    Add additional arguments to Ridge regressor

--- model/regressor.py
+++ model/regressor.py
@@ -5,7 +5,7 @@ from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 
 
-def get_regressor(model, alpha=1, c=1, epsilon=0.1, verbose=0, jobs=-1):
+def get_regressor(model, alpha=1.0, c=1.0, epsilon=0.1, verbose=0, jobs=-1):
     """Initializes a new regressor.
 
     Parameters:
@@ -23,7 +23,8 @@ def get_regressor(model, alpha=1, c=1, epsilon=0.1, verbose=0, jobs=-1):
         return LinearRegression(
             fit_intercept=False, normalize=False, copy_X=False, n_jobs=jobs)
     elif model == 'ridge':
-        return Ridge(alpha=alpha)
+        return Ridge(
+            alpha=alpha, fit_intercept=False, normalize=False, copy_X=False)
     elif model == 'svr':
         return SVR(C=c, epsilon=epsilon, verbose=verbose)
     elif model == 'random-forest':
